083d051ef6686317908ee90d1a085441bcc95b73
Glen Correia <glen_correia@outlook.com>
2024-05-20 21:55:33 +0530

    Code to check if negative number

--- string_calculator.rb
+++ string_calculator.rb
@@ -17,10 +17,19 @@ class StringCalculator
 				end
 			end
 			number_parts = numbers.split(delimiter).map(&:to_i)
+			n_numbers = number_parts.select { |num| num < 0 }
+			if n_numbers.any?
+				negative_number n_numbers, 2
+			end
 			number_parts_summed = number_parts.sum
 			output = number_parts_summed
 		else
-			output = numbers.to_i
+			number = numbers.to_i
+			if number < 0
+				negative_number number, 1
+			else
+				output = numbers.to_i
+			end
 		end
 		puts output
 		return output
@@ -34,6 +43,21 @@ class StringCalculator
 		end
 	end
 
+	def self.negative_number(numbers, type)
+		if type == 2
+			numbers = numbers.join(', ')
+		end
+		raise ArgumentError, "negative numbers not allowed #{numbers}"
+	end
+
 end
 
-StringCalculator.add("//!\n23!23!23")
+StringCalculator.add("") # 0
+StringCalculator.add("12") # 12
+StringCalculator.add("13,20") # 33 
+StringCalculator.add("1\n4,5") # 10
+StringCalculator.add("1,\n") # Nil
+StringCalculator.add("//-\n1-2-3-4") # 10 
+# StringCalculator.add("-2") # ArgumentError
+# StringCalculator.add("1, -2, 3, 4, -5") # ArgumentError
+
