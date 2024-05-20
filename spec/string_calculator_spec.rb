67486d2a492623db9f1832f1153c04f777e56489
Glen Correia <glen_correia@outlook.com>
2024-05-20 18:59:38 +0530

    Test cases to check delimiters in string and add likewise

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -21,5 +21,13 @@ describe StringCalculator do
 		it 'invalidates input that has newline character at the end of the string' do
 			expect(StringCalculator.add("1,\n")).to eq(nil)
 		end
+
+		it 'checks for delimiter v1' do
+			expect(StringCalculator.add("//;\n1;2")).to eq(3)
+		end
+
+		it 'checks for delimiter v2' do
+			expect(StringCalculator.add("//-\n1-2-3-4")).to eq(10)
+		end
   end
 end
