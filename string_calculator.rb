56b03274c833ec87889a195ae233c7aa02812929
Glen Correia <glen_correia@outlook.com>
2024-05-20 18:02:13 +0530

    Add code to replace newline characters with comma

--- string_calculator.rb
+++ string_calculator.rb
@@ -6,6 +6,9 @@ class StringCalculator
 		if numbers.strip == ""
 			output = 0
 		elsif numbers.include? ','
+			if numbers.include? "\n"
+				numbers = sanitize_string(numbers) 
+			end
 			number_parts = numbers.split(',').map(&:to_i)
 			number_parts_summed = number_parts.sum
 			output = number_parts_summed
@@ -16,6 +19,10 @@ class StringCalculator
 		return output
 	end
 
+	def self.sanitize_string(numbers)
+		numbers.sub! "\n", ","
+	end
+
 end
 
-StringCalculator.add("12,14")
+StringCalculator.add("12\n2,3")
